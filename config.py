ba048aff7311d21dffbc24c66ae070836066b5ca
vonatlus <52702565+adil-sultanov@users.noreply.github.com>
2023-01-17 16:30:35 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-feedback_chatID = -845640938
+feedback_chatID = 123456
+
+Google_credentials_key = 'XXXXXX.json'
 
 welcome = 'This bot can suggest emoji(s) based on image uploaded using Google Cloud Vision API.\n\nTry it out, upload a photo no larger than 20 mb.'
 about = 'This bot can suggest emoji(s) based on image uploaded using Google Cloud Vision API. It scans for key objects and faces.\n\nAuthor: Adil Sultanov\nTelegram: @vonatlus\n<a href="https://github.com/adil-sultanov/image-to-emoji-bot">🔗Github link</a>'
 
-TOKEN = "XXXXXXXXXXXXX"
+TOKEN = '12345:XXXXXX'
